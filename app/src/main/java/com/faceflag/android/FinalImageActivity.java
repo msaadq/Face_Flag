5eb9c1f8d3bedc38e875fc001c983c8d6479054c
Usman Mahmood <usmanwardag@gmail.com>
2016-02-02 21:12:14 +0500

    Adds option to save images in sd card

--- app/src/main/java/com/faceflag/android/FinalImageActivity.java
+++ app/src/main/java/com/faceflag/android/FinalImageActivity.java
@@ -1,22 +1,41 @@
 package com.faceflag.android;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class FinalImageActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.final_image_activity);
 
-        ImageView imageView = (ImageView) findViewById(R.id.image);
+        imageView = (ImageView) findViewById(R.id.image);
         imageView.setImageResource(R.drawable.bg_image);
 
     }
@@ -35,32 +54,19 @@ public class FinalImageActivity extends AppCompatActivity {
 
         // Handle item selection
         switch (item.getItemId()) {
+
             case R.id.menu_item_share:
-
-                Intent sendIntent = new Intent();
-                sendIntent.setAction(Intent.ACTION_SEND);
-                Uri uri = Uri.parse("android.resource://com.faceflag.android/drawable/bg_image");
-                sendIntent.putExtra(Intent.EXTRA_STREAM, uri);
-                sendIntent.putExtra(Intent.EXTRA_TEXT, "I am supporting Peshawar Zalmi! Get " +
-                        "your team's flag here: http://playstore.android.com/FaceFlag");
-                sendIntent.setType("image/*");
-                startActivity(Intent.createChooser(sendIntent, "share"));
-
+                shareImage();
                 return true;
 
             case R.id.menu_item_save:
+
+                saveImage();
                 return true;
 
             case R.id.menu_item_delete:
 
-                //Create intent
-                Intent intent = new Intent(FinalImageActivity.this, PhotoActivity.class);
-                Toast.makeText(this, "Image has been deleted. Try out a new one!",
-                        Toast.LENGTH_LONG).show();
-
-                //Start Photo activity
-                startActivity(intent);
-
+                deleteImage();
                 return true;
 
             default:
@@ -68,5 +74,72 @@ public class FinalImageActivity extends AppCompatActivity {
         }
     }
 
+    private void saveImage() {
+        BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
+        Bitmap bitmap = drawable.getBitmap();
+
+        File sdCardDirectory = Environment.getExternalStorageDirectory();
+        File image = new File(sdCardDirectory, "FaceFlag.png");
+
+        boolean success = false;
+        int permission = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    this,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        } else {
+            Log.v("Coming here", "sjjs");
+            // Encode the file as a PNG image.
+            FileOutputStream outStream;
+            try {
+
+                outStream = new FileOutputStream(image);
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, outStream);
+                /* 100 to keep full quality of the image */
+
+                outStream.flush();
+                outStream.close();
+                success = true;
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (success) {
+                Toast.makeText(getApplicationContext(), "Image saved with success",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getApplicationContext(),
+                        "Error during image saving", Toast.LENGTH_LONG).show();
+            }
+
+        }
+    }
+
+    private void shareImage() {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        Uri uri = Uri.parse("android.resource://com.faceflag.android/drawable/bg_image");
+        sendIntent.putExtra(Intent.EXTRA_STREAM, uri);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "I am supporting Peshawar Zalmi! Get " +
+                "your team's flag here: http://playstore.android.com/FaceFlag");
+        sendIntent.setType("image/*");
+        startActivity(Intent.createChooser(sendIntent, "share"));
+    }
+
+    private void deleteImage(){
+        //Create intent
+        Intent intent = new Intent(FinalImageActivity.this, PhotoActivity.class);
+        Toast.makeText(this, "Image has been deleted. Try out a new one!",
+                Toast.LENGTH_LONG).show();
+
+        //Start Photo activity
+        startActivity(intent);
+    }
 
 }
